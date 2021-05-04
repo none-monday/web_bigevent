cc46850c6812a383a2caa88af724dbf557b97a54
none-monday <2401833808@qq.com>
2021-05-04 12:49:24 +0800

    完成了主页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,16 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    //设置headers请求头
+    if(options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //统一配置complete
+    options.complete = function(res) {
+       if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+       }
+    }
 })
